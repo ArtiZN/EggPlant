85db44ee73d4cbb52999aa7df5f0db92eb6575b3
SonyaMarmeladova <7exalyed7@gmail.com>
2018-10-06 21:38:11 +0300

    create database component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,6 +4,7 @@ import { MatTabsModule } from '@angular/material/tabs';
 
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
+import { DatabaseComponent } from './components/database/database.component';
 
 @NgModule({
     imports: [
@@ -12,7 +13,8 @@ import { HomeComponent } from './components/home/home.component';
     ],
     declarations: [
         HeaderComponent,
-        HomeComponent
+        HomeComponent,
+        DatabaseComponent
     ],
     exports: [
 
