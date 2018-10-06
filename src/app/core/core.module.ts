f6725afb77b2c9d4ffa5219364e6baed131199c8
SonyaMarmeladova <7exalyed7@gmail.com>
2018-10-06 19:51:13 +0300

    add core module

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+    imports: [
+        BrowserModule
+    ],
+    declarations: [
+        
+    ],
+    exports: [
+
+    ],
+    providers: []
+})
+export class CoreModule { }
