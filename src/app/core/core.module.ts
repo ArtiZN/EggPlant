3fa030d7495edd51c50cf5131a5c3fa52f2b5e1e
SonyaMarmeladova <7exalyed7@gmail.com>
2018-10-07 00:13:21 +0300

    add ag-grid tabel

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
+import { AgGridModule } from 'ag-grid-angular';
 
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
@@ -12,7 +13,8 @@ import { BottomPanelComponent } from './components/bottom-panel/bottom-panel.com
 @NgModule({
     imports: [
         BrowserModule,
-        MatTabsModule
+        MatTabsModule,
+        AgGridModule.withComponents([])
     ],
     declarations: [
         HeaderComponent,
