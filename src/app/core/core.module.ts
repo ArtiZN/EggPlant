0ca80df633a4b67c26cf488e1c9a45cbe7dccb46
SonyaMarmeladova <7exalyed7@gmail.com>
2018-10-06 19:53:52 +0300

    create navigation component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
     imports: [
         BrowserModule
     ],
     declarations: [
-        
+        HeaderComponent
     ],
     exports: [
 
