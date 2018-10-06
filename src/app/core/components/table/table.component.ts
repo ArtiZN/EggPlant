30bcfd449a2e185f7246b75c3a5a426f3ac4540c
SonyaMarmeladova <7exalyed7@gmail.com>
2018-10-07 01:48:57 +0300

    enable enterprise

--- src/app/core/components/table/table.component.ts
+++ src/app/core/components/table/table.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { AgGridNg2 } from 'ag-grid-angular';
 
 @Component({
   selector: 'app-table',
@@ -7,6 +8,8 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit {
+
+  @ViewChild('agGrid') agGrid: AgGridNg2;
 
   columnDefs = [
     {headerName: 'Make', field: 'make', checkboxSelection: true  },
@@ -18,8 +21,14 @@ export class TableComponent implements OnInit {
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.rowData = this.http.get('https://api.myjson.com/bins/15psn9');
-    console.log(this.rowData);
+    this.rowData = this.http.get('https://api.myjson.com/bins/ly7d1');
   }
 
+  getSelectedRows() {
+    const selectedNodes = this.agGrid.api.getSelectedNodes();
+    const selectedData = selectedNodes.map( node => node.data );
+    const selectedDataStringPresentation = selectedData.map( node => node.make + ' ' + node.model).join(', ');
+    console.log(`Selected nodes: ${selectedDataStringPresentation}`);
+    console.log(this.agGrid);
+  }
 }
