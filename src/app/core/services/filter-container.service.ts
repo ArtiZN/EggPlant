2b8a435ba3878e1b22b4b137f94f0b5d8dd7d649
SonyaMarmeladova <7exalyed7@gmail.com>
2018-10-10 21:07:11 +0300

    create methods

--- src/app/core/services/filter-container.service.ts
+++ src/app/core/services/filter-container.service.ts
@@ -4,6 +4,15 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class FilterContainerService {
+  private _filterContainer = [];
 
   constructor() { }
+
+  get filterContainer() {
+    return this._filterContainer;
+  }
+
+  push(item) {
+    this._filterContainer.push(item);
+  }
 }
