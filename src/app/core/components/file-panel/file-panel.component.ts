b2132b635f04b17770c79b09c5a151fa28d4dfea
KPbICA-CTATUCT <z.artemznahar@mail.ru>
2018-10-21 16:31:11 +0300

    addfilereader

--- src/app/core/components/file-panel/file-panel.component.ts
+++ src/app/core/components/file-panel/file-panel.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import * as XLSX from 'xlsx'; 
 
 @Component({
   selector: 'app-file-panel',
@@ -9,7 +10,37 @@ export class FilePanelComponent implements OnInit {
 
   constructor() { }
 
+  @ViewChild('fileImportInput')
+  fileImportInput: ElementRef;
+
   ngOnInit() {
   }
 
+  fileReset() {
+    this.fileImportInput.nativeElement.value = "";
+  }
+
+  fileChangeListener($event) {
+    const target: DataTransfer = <DataTransfer>($event.target);
+
+    if (target.files.length !== 1) { 
+      throw new Error('Cannot use multiple files');
+    }
+
+    const reader: FileReader = new FileReader();
+	    reader.onload = (e: any) => {
+	    	const bstr: string = e.target.result;
+			  const wb: XLSX.WorkBook = XLSX.read(bstr, {type: 'binary'});
+
+			  const wsname: string = wb.SheetNames[0];
+			  const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+
+ 			  let excelData: any = XLSX.utils.sheet_to_json(ws, {header: 1});
+ 			  console.log(excelData);
+
+        // this.currentFileName = target.files[0]["name"];
+        this.fileReset();
+	    };
+	    reader.readAsBinaryString(target.files[0]);
+  }
 }
