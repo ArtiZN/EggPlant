82175474b2ac7479cd32fbe1c0f5d9f6815e1769
KPbICA-CTATUCT <z.artemznahar@mail.ru>
2018-11-10 17:42:31 +0200

    add one more route

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,3 +1,4 @@
+import { LoginComponent } from './core/components/login/login.component';
 import { HomeComponent } from './core/components/home/home.component';
 
 import { Routes, RouterModule } from "@angular/router";
@@ -5,6 +6,9 @@ import { Routes, RouterModule } from "@angular/router";
 const routes: Routes = [
 	{
 		path: "", component: HomeComponent
+	},
+	{
+		path: "login", component: LoginComponent
 	}
 ]
 export const routing = RouterModule.forRoot(routes);
